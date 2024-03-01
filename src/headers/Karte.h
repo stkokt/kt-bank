3e5fc895ba4014d8b1214fd53707b69e9a4464ce
JanaFiege <jana-fiege@t-online.de>
2024-03-01 11:06:51 +0100

    erster Versuch Karte
    Inkludierung unklar, muss noch gefixt werden

--- src/headers/Karte.h
+++ src/headers/Karte.h
@@ -2,19 +2,18 @@
 #define KARTE_H */
 #pragma once
 #include "Bankkunde.h"
+#include "Kreditkarte.h"
 
+class Karte {
+ protected:
+  Karte();  // Konstruktor protected setzen um Instanzierung zu verhindern => nur Unterklassen (Kredit/Giro) instanzieren
 
+ private:
+  bool validKarte;  // Korrekte Überprüfung (PIN/PZ)
+  bool kartenTyp;   // Kreditkarte: true, Girokarte: false
 
-class Karte{
-    protected:
-        Karte(); //Konstruktor protected setzen um Instanzierung zu verhindern => nur Unterklassen (Kredit/Giro) instanzieren
-
-    private:
-        bool validKarte; //Korrekte Überprüfung (PIN/PZ)
-        bool kartenTyp;  //Kreditkarte: true, Girokarte: false
-
-    public:
-        bool getKartenTyp();
-        bool getValidKarte();
-        bool checkKarte(); //Setter für validKarte
-}
+ public:
+  bool getKartenTyp();
+  bool getValidKarte();
+  bool checkKarte();  // Setter für validKarte
+};
